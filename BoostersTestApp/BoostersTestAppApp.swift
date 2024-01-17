045e636a60067a4a25f6382e29196ae44163bbbd
Artem Zabihailo <yatimistark@gmail.com>
2024-01-17 14:06:44 +0200

    Initial Commit

---
+++ BoostersTestApp/BoostersTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoostersTestAppApp.swift
+//  BoostersTestApp
+//
+//  Created by Artem Zabihailo on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BoostersTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
